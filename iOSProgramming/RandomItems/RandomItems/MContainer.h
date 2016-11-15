5dd330a00dd93e62af43ef8362553f9c51dad33b
XFeiF <eva.aeolus@gmail.com>
2016-11-16 00:07:09 +0800

    Chapter 2 Finished~😊

---
+++ iOSProgramming/RandomItems/RandomItems/MContainer.h
@@ -0,0 +1,17 @@
+//
+//  MContainer.h
+//  RandomItems
+//
+//  Created by XFeiF on 2016/11/15.
+//  Copyright © 2016年 NodeX. All rights reserved.
+//
+
+#import "MItem.h"
+
+@interface MContainer : MItem
+
+@property (nonatomic) NSMutableArray *subitems;
+
+//-(int)sumValue;
+
+@end
